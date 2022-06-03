9026741c4d326b5786e29f3873f36a357442240a
SauravChakers <102030629+SauravChakers@users.noreply.github.com>
2022-06-03 09:59:41 +0400

    Add files via upload

---
+++ my_first_code.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#This is my first line of code
+print("My name is Saurav Chakraborty")
+print("The date today is 3 June 2022")
+
+
+# In[2]:
+
+
+#Write a program to analyse simple data from 1.2.5
+a=45000
+b=23400
+c=67000
+d=34600
+e=12900
+
+#Add all sales
+total_sales = a+b+c+d+e
+
+#Average sales for December
+average_sales = total_sales/5
+
+#Calculate Missing Property price
+missing_property_price = c-6500
+
+#View outputs
+print(total_sales)
+print(average_sales)
+print(missing_property_price)
+
+
+# In[ ]:
+
+
+
+
